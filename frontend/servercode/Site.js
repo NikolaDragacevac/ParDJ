d8f9aa5d923f3e9acc1b13ea086e4128147d5aac
Ramit Jogi <ramit123@live.co.uk>
2019-02-03 11:57:12 +0000

    F mhy Fing F

--- frontend/servercode/Site.js
+++ frontend/servercode/Site.js
@@ -1,15 +1,23 @@
 const express = require('express');
 var data = require("../src/Util/Servers.json")
 const app = express()
+var cors = require('cors')
 const bodyParser = require('body-parser')
 
  app.use(bodyParser.json());
 
-app.post('/', (req, res) => {
-    console.log(req.Host)
+ app.post('/*', cors(), (req, res) => {
+    console.log(JSON.stringify(req.body))
     console.log("sending data")
 
     res.send("heres the response!");
+});
+
+app.get('/', cors(), (req, res) => {
+    console.log(JSON.stringify(req.body))
+    console.log("sending data")
+
+    res.header('content-type', 'application/json').send("heres the response!");
 });
 
 app.listen(3001, () => console.log(`Listening on port 3001!`))
