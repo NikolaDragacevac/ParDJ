b2b902b631e24eccdee7c89cdf72c4d533becf01
Ramit Jogi <ramit123@live.co.uk>
2019-02-05 12:49:05 +0000

    Moved gitIgnore

--- Client(IoT Device)/ClientNode.js
+++ Client(IoT Device)/ClientNode.js
@@ -1,6 +1,15 @@
 var net = require('net');
 
-var client = new net.Socket();
+const client = net.createConnection({ port: 2999 }, () => {
+  // 'connect' listener
+  console.log('connected to server!');
+  client.end();
+});
+
+client.on('end', () => {
+  console.log('disconnected from server');
+});
+
 client.connect(2999, '172.20.10.11', function() {
     console.log('Connected');
     client.end();
--- frontend/servercode/ServerNode.js
+++ frontend/servercode/ServerNode.js
@@ -7,17 +7,15 @@ console.log(servers);
 const server = net.createServer((c) => {
     // 'connection' listener
     servers.push(c.remoteAddress);
-    console.log(servers[0]);
-    c.on('end', () => {
-        console.log('client disconnected');
-    });
     fs.writeFile("../src/Util/Servers.json", JSON.stringify(servers), function(err) {
         if(err) {
             return console.log(err);
         }
-        console.log("The file was saved!");
     });
-  c.pipe(c);
+    
+    c.on('end', () => {
+      console.log('client disconnected');
+  });
 });
 
 server.listen(2999, () => {
